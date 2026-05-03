28510abc5f2b52cd5733304d584ffeb16257be47
alexmalambi <malambialex@gmail.com>
2026-05-03 21:34:54 +0100

    chore(deps): support python3 environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,6 +11,7 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 
 from app.database.base import Base
 from app.database.database import get_db
@@ -23,6 +24,7 @@ SQLALCHEMY_TEST_URL = "sqlite:///:memory:"
 test_engine = create_engine(
     SQLALCHEMY_TEST_URL,
     connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
 )
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
